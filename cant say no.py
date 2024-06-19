a527792151647fc950b66bc64b2c4bfac8a56d88
HardikShreays <72156209+HardikShreays@users.noreply.github.com>
2024-06-19 23:25:11 +0530

    Add files via upload

---
+++ cant say no.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+import random
+
+
+def yes():
+    global win
+    messagebox.showinfo("yay!", "yay!! Thank you !love you <3")
